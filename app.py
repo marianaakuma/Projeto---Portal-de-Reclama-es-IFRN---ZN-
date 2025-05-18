4c3ab2a421d6d6d7436eba997404ea2f1894e0ee
marianaakuma <153663415+marianaakuma@users.noreply.github.com>
2025-05-18 00:53:36 +0000

    17/05/25

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/')
 def login():
     return render_template('login.html')
 
